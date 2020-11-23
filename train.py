dfffad9eba5558c7e61c06273612c6453fd30af3
AndreiToroplean <andrei.toroplean@gmail.com>
2020-11-23 23:08:26 +0100

    Tuned the model's hyperparameters to minimize the final loss and the number of epochs needed to achieve it.

--- train.py
+++ train.py
@@ -8,13 +8,13 @@ def main():
     dataset = tf.data.Dataset.from_tensor_slices((x, y))
     dataset = dataset.batch(32, drop_remainder=True)
 
-    model_dir = r"models\model_03"
+    model_dir = r"models\model_10"
     try:
         model = tf.keras.models.load_model(model_dir)
     except OSError:
         model = create_and_train_model(dataset, model_dir)
 
-    x_eval = tf.range(-10.0, 10.0, 0.1)
+    x_eval = tf.range(-15.0, 15.0, 0.1)
     test_dataset = tf.data.Dataset.from_tensor_slices(x_eval)
     test_dataset = test_dataset.batch(32)
     y_predict = model.predict(test_dataset)
@@ -22,26 +22,29 @@ def main():
     plt.figure()
     plt.plot(x, y, ".")
     plt.plot(x_eval.numpy(), y_predict, "-")
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
+    plt.xlim(-15, 15)
+    plt.ylim(-15, 15)
     plt.show()
 
 
 def create_and_train_model(dataset, model_dir):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(100, activation="relu", input_shape=(1,)),
-        tf.keras.layers.Dense(1),
-        ])
+    depth = 6
+    width = 64
+
+    model = tf.keras.models.Sequential([tf.keras.layers.Dense(width, activation="relu", input_shape=(1, ))])
+    for _ in range(depth - 1):
+        model.add(tf.keras.layers.Dense(width, activation="relu"))
+    model.add(tf.keras.layers.Dense(1))
 
     model.compile(
         optimizer="adam",
-        loss=tf.keras.losses.MeanSquaredError(),
+        loss=tf.keras.losses.MeanAbsoluteError(),
         )
 
     model.fit(
         dataset,
-        epochs=100,
-        callbacks=[tf.keras.callbacks.EarlyStopping("loss", patience=5)],
+        epochs=512,
+        callbacks=[tf.keras.callbacks.EarlyStopping("loss", patience=32, restore_best_weights=True)],
         )
 
     model.save(model_dir)
